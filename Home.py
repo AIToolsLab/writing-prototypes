1c08bbf2793621b951c2ebc94612ad7b2d4644a5
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2024-05-22 16:10:30 -0400

    Nav menu clarity

--- Home.py
+++ Home.py
@@ -2,5 +2,8 @@ import streamlit as st
 
 st.title("Writing Tools Prototypes")
 
-st.page_link("pages/1_Rewrite.py", label="Rewrite with predictions")
-st.page_link("pages/2_Highlights.py", label="Highlight locations for possible edits")
+st.markdown("Click one of the links below to see a prototype in action.")
+
+st.page_link("pages/1_Rewrite.py", label="Rewrite with predictions", icon="📝")
+st.page_link("pages/2_Highlights.py", label="Highlight locations for possible edits", icon="🖍️")
+
