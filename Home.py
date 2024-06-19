b9bb50f9e54a1011fe6682dd40a1f73a7fef318e
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2024-06-19 09:56:20 -0400

    Add note about data privacy in Home.py

--- Home.py
+++ Home.py
@@ -7,3 +7,4 @@ st.markdown("Click one of the links below to see a prototype in action.")
 st.page_link("pages/1_Rewrite.py", label="Rewrite with predictions", icon="📝")
 st.page_link("pages/2_Highlights.py", label="Highlight locations for possible edits", icon="🖍️")
 
+st.markdown("*Note*: These services send data to a remote server for processing. The server logs requests. Don't use sensitive or identifiable information on this page.")
